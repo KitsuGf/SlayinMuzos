582057706dc6ef743e3f93832e07ee335384a352
Juan Miguel <kitsu_pkiller@hotmail.com>
2020-02-11 20:38:28 +0100

    - Added in assets new TMX map to castle. Code Changes: - Update code to see the tmx map in androidstudio.

--- core/src/com/salyin/muzos/Main.java
+++ core/src/com/salyin/muzos/Main.java
@@ -25,10 +25,10 @@ public class Main extends ApplicationAdapter {
 
 		float w = Gdx.graphics.getWidth(); //Obtenemos la anchura de nuestra pantalla
 		float h = Gdx.graphics.getHeight(); //Obtenemos la atura de nuestra pantalla
-		TiledMap map = new TmxMapLoader().load("maps/rooms/room_jungle/prueba.tmx"); //Cargamos el tilemap desde assets
+		TiledMap map = new TmxMapLoader().load("maps/rooms/room_castle/castle_room.tmx"); //Cargamos el tilemap desde assets
 		renderer = new OrthogonalTiledMapRenderer(map, unitScale); //Establecemos el renderizado del mapa dividido en Tiles de 32 dp.
 		camera = new OrthographicCamera(); //Declaramos la cámara a través de la que veremos el mundo
-		camera.zoom=0.80f; //Establecemos el zoom de la cámara. 0.1 es más cercano que 1. Jugaremos con esto en clase
+		//camera.zoom=0.80f; //Establecemos el zoom de la cámara. 0.1 es más cercano que 1. Jugaremos con esto en clase
 		WIDTH = ((TiledMapTileLayer) map.getLayers().get(0)).getWidth(); //Obtenemos desde el mapa el número de tiles de ancho de la 1º Capa
 		HEIGHT = ((TiledMapTileLayer) map.getLayers().get(0)).getHeight(); //Obtenemos desde el mapa el número de tiles de alto de la 1º Capa
 		Gdx.app.log("Width",Float.toString(WIDTH)); //Sacamos por el log el número de tiles de ancho
@@ -36,8 +36,8 @@ public class Main extends ApplicationAdapter {
 		camera.setToOrtho(false, WIDTH,HEIGHT); //Establecemos la cámara, y le decimos cuanto tiene que ocupar.
 		Gdx.app.log("camera x",Float.toString(camera.position.x));
 		Gdx.app.log("camera x",Float.toString(camera.position.y));
-		camera.position.y = Math.max(camera.position.y -10,0);
-		camera.position.x = Math.max(camera.position.x +4,0);
+		camera.position.y = Math.max(camera.position.y -6f,0);
+		//camera.position.x = Math.max(camera.position.x +4,0);
 		camera.update(); //Colocamos la Cámara
 
 	}
