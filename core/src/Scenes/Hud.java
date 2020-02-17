310cfeb9a80145a94faf4e920786f6e54af13209
KitsuGf <kitsu_pkiller@hotmail.com>
2020-02-17 14:06:32 +0100

    Added: - Trying to make background hud with no success. TODO FIX IT.

--- core/src/Scenes/Hud.java
+++ core/src/Scenes/Hud.java
@@ -10,12 +10,15 @@ package Scenes;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -29,6 +32,9 @@ public class Hud implements Disposable {
     private Integer countCombo;
     private Image img;
     private Integer countCoins;
+    private Texture texture;
+    private TextureRegion textR;
+    private TextureRegionDrawable textRd;
 
     Label lbCombo;
     Label lbEnemy;
@@ -44,11 +50,23 @@ public class Hud implements Disposable {
         countCombo = 0;
         countCoins = 0;
 
+
+
+
+
         viewport = new StretchViewport(Main.V_WIDTH, Main.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, sb);
 
+        //TODO ARREGLAR BACKGROUND HUD
+        texture = new Texture("sprites/hud/hud_controllers/test_hud.png");
+        textR = new TextureRegion(texture);
+        textRd = new TextureRegionDrawable(textR);
+
+
+        img = new Image();
         Table tb = new Table();
         tb.top();
+        tb.debug();
         tb.setFillParent(true);
 
         lbTxEnemy = new Label(String.format("Enemies:"), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
@@ -71,6 +89,8 @@ public class Hud implements Disposable {
 
     @Override
     public void dispose() {
+
         stage.dispose();
     }
+
 }
